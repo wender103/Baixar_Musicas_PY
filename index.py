d61ca1c763e1905ff8c17fb14b6e98bbcc4a728f
wender103 <wendertestes01@gmail.com>
2024-09-25 14:16:37 -0300

    pequena mudança

--- index.py
+++ index.py
@@ -176,4 +176,5 @@ def download_and_analyze():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=3001)
+    port = int(os.environ.get("PORT", 3001))
+    app.run(host='0.0.0.0', port=port)
