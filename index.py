a0093b9559e8ff04fd88a93ec142488a3ef1c390
wender103 <wendertestes01@gmail.com>
2024-09-27 11:05:20 -0300

    Será que vai dessa vez?

--- index.py
+++ index.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Inicializa o Firebase
 try:
     cred = credentials.Certificate({
         "type": os.getenv("FIREBASE_TYPE"),
@@ -26,71 +27,58 @@ try:
         "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
         "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
         "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+        "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")  # Adicionado
     })
-    firebase_admin.initialize_app(cred, {'storageBucket': 'botdc-e412e.appspot.com'})
+    firebase_admin.initialize_app(cred, {'storageBucket': 'melowave-f6f7c.appspot.com'})
 except Exception as e:
     print(f"Erro ao inicializar o Firebase: {e}")
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS"]}})
 
+# Funções auxiliares
 def obter_data_atual():
-    """Retorna a data atual no formato DD/MM/YYYY."""
     return datetime.now().strftime("%d/%m/%Y")
 
 def resize_image(image, size):
-    """Redimensiona a imagem mantendo o formato quadrado."""
     image = image.convert("RGB")
     width, height = image.size
-
     new_size = min(width, height)
     left = (width - new_size) / 2
     top = (height - new_size) / 2
     right = (width + new_size) / 2
     bottom = (height + new_size) / 2
-
     image = image.crop((left, top, right, bottom))
     image = image.resize(size, Image.LANCZOS)
     return image
 
 def extract_colors(image, num_colors=7):
-    """Extrai as cores principais de uma imagem usando colorgram."""
     colors = colorgram.extract(image, num_colors)
     sorted_colors = sorted(colors, key=lambda c: c.proportion, reverse=True)
     return [rgb_to_hex(color.rgb.r, color.rgb.g, color.rgb.b) for color in sorted_colors]
 
 def rgb_to_hex(r, g, b):
-    """Converte valores RGB em formato HEX."""
     return "#{:02x}{:02x}{:02x}".format(r, g, b)
 
 def upload_to_firebase(image, folder_name, file_name):
-    """Faz o upload da imagem para o Firebase Storage dentro de uma pasta."""
     bucket = storage.bucket()
     blob = bucket.blob(f'MusicasPostadas/{folder_name}/{file_name}')
-    
     buffer = BytesIO()
     image.save(buffer, format="JPEG")
     buffer.seek(0)
-
     blob.upload_from_file(buffer, content_type='image/jpeg')
     blob.make_public()
-    
     return blob.public_url
 
 def update_music_data(data, document_id):
-    """Atualiza o array de músicas no Firestore dentro de um documento existente."""
     db = firestore.client()
     music_ref = db.collection('Musicas').document(document_id)
-
-    # Obtém o documento existente
     doc = music_ref.get()
     if doc.exists:
         existing_data = doc.to_dict()
         music_list = existing_data.get('Musicas', [])
         music_list.append(data)
-
-        # Atualiza o array no documento
         music_ref.update({'Musicas': music_list})
     else:
         return jsonify({'error': 'Documento não encontrado!'}), 404
@@ -106,12 +94,11 @@ def download_and_analyze():
 
     try:
         folder_id = str(uuid.uuid4())
-
         ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': '%(title)s.%(ext)s',
         }
-        
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=True)
             audio_file_name = ydl.prepare_filename(info)
@@ -141,16 +128,13 @@ def download_and_analyze():
         
         if response.status_code == 200:
             image = Image.open(BytesIO(response.content))
-
             extracted_colors = extract_colors(image)
 
             image_data = []
             for size_label, size in sizes.items():
                 resized_image = resize_image(image, size)
-
                 file_name = f'{size_label}_{video_id}_{str(uuid.uuid4())}.jpg'
                 public_url = upload_to_firebase(resized_image, folder_id, file_name)
-
                 width, height = size
                 image_data.append({
                     'url': public_url,
@@ -171,7 +155,7 @@ def download_and_analyze():
                 "ID": folder_id,
                 "Imagens": [img['url'] for img in image_data],
                 "Img": image_data[-1]['url'] if image_data else "",
-                "Letra": [],  # Aqui você pode adicionar a lógica para pegar a letra da música
+                "Letra": [],  
                 "Nome": yt.title,
                 "VideoURL": video_url,
                 "Views": yt.views or 0
